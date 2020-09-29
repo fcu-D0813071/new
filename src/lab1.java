7fd2a415def4350c8bd0a96026cd052a38fba50a
user <user@EF06>
2020-09-29 09:33:31 +0800

    hw1

--- src/lab1.java
+++ src/lab1.java
@@ -4,7 +4,9 @@ public class lab1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello OOP");
+		System.out.println("I'm ´¿¤l·¶");
+		System.out.println("My nid is D0813071");
+		System.out.println("This is lab1 hw");
 		
 	}
 	
